930a8977528e018f63203742bd1a70fd7958feb1
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-09-10 20:07:05 +0500

    Task1

---
+++ Tyuiu.GolovanovDO.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Данила";
+        int age = 18;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age);
+
+        Console.ReadKey();
+    }
+}   

d9b47fd5f5a5b6bea8f9a69b8cef84444f2b45c8
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-09-13 21:46:23 +0500

    Task5

---
+++ Tyuiu.GolovanovDO.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.GolovanovDO.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+            { return a + b; }
+
+        public static int Substraction(int a, int b) 
+            { return a - b; }
+
+        public static int Multiplication(int a, int b)
+            { return a * b; }
+
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                //Пример создан в целях демонстрации ветвления
+                // В Реальных проектах нужно использовать Exception
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            
+            }
+        }
+    }
+}
---
+++ Tyuiu.GolovanovDO.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GolovanovDO.Sprint0.Task5.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+
+        Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+
+        Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+
+        //пример развлетвляющейся структуры
+        Console.WriteLine("A / B = " + DataService.Division(9, 3));
+        Console.ReadKey();
+    }
+}

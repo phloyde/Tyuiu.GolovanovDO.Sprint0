9e0b634a59281839321bf9f71133b4b9edc2f89d
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-09-16 21:52:02 +0500

    Task6

---
+++ Tyuiu.GolovanovDO.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GolovanovDO.Sprint0.Task6.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numsArray = new int[] { 1, 2, 3, 4, 5, };
+        //Пример циклической структуры находится в библиотеке классов в методе AdditionArray
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+        //Пример циклической структуры находится в библиотеке классов в методе SubstractionArray
+        Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+
+        //Пример циклической структуры находится в библиотеке классов в методе MultiplicationArray
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));    
+
+    }
+}
